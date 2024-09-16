b4a4b48dcd73d9eb6ff38440c975883887a34093
stepangracev <1800687@mail.ru>
2024-09-16 12:01:39 +0300

    Added arrow icon

--- app/src/main/java/com/example/newslistapp/presentation/screen/components/NewsItem.kt
+++ app/src/main/java/com/example/newslistapp/presentation/screen/components/NewsItem.kt
@@ -1,12 +1,15 @@
 package com.example.newslistapp.presentation.screen.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
@@ -61,6 +64,15 @@ fun NewsItem(news: NewsModel, modifier: Modifier = Modifier) {
                     color = Color.White
                 )
             }
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_arrow),
+                contentDescription = null,
+                modifier = Modifier.size(36.dp)
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 20.dp, end = 20.dp),
+                contentScale = ContentScale.FillBounds
+            )
         }
     }
 }
--- app/src/main/java/com/example/newslistapp/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newslistapp/presentation/viewmodel/NewsViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.newslistapp.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newslistapp.domain.INewsRepository
